147a108cfac79479ae2c0cd1853ca96388b18b64
WexyR <romain.biannic2@gmail.com>
2020-03-13 00:27:03 +0100

    redesigned slider cards

--- js/slider_behavior.js
+++ js/slider_behavior.js
@@ -4,24 +4,16 @@ $(function (){
 
   $cards.on("mouseenter", function(e){
     $(this).children().addClass("activeCard");
-    $(this).children("img").css("border", "3px solid rgba(8, 146, 208, 0.99)");
-    $(this).children("img").css("opacity", "0.2");
-    $(this).children("img").css("filter", "grayscale(100%)");
-    $(this).children("article").css("opacity", '1');
 
-    $(this).children().on("click", function(e){
-      alert("website in progress!");
-    });
+    // $(this).children().on("click", function(e){
+    //   alert("website in progress!");
+    // });
   });
 
   $cards.on("mouseleave", function(e){
     $(this).children().removeClass("activeCard");
-    $(this).children("img").css("border", "none");
-    $(this).children("img").css("opacity", "1");
-    $(this).children("img").css("filter", "grayscale(0%)");
-    $(this).children("article").css("opacity", '0');
 
-    $(this).children().off("click");
+    // $(this).children().off("click");
   });
 
 });
